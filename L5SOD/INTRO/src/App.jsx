a23d12d1dcbaaa00803303747ad434057667b0a7
Andy <ishmukazi@gmail.com>
2025-09-09 11:44:56 +0200

    simple starting P

--- L5SOD/INTRO/src/App.jsx
+++ L5SOD/INTRO/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './index.css'
+import imageOne from '../public/1.jpg'
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <div>
+            <header className="header">
+                <nav className="navbar">
+                   <p>Andy</p>
+                    <div>
+                        <a href="#">About</a>
+                        <a href="#">Experience</a>
+                        <a href="#">Project</a>
+                        <a href="#">Contact</a>
+                    <img src={imageOne} alt="" className="imagesone" />
+                    </div>
+                </nav>
 
-export default App
+            </header>
+            <body>
+                <div className="hero">
+                    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsum soluta excepturi sapiente expedita a veniam rerum magnam quasi dolores velit sunt asperiores nesciunt exercitationem inventore, praesentium maxime ipsam laboriosam.</p>
+                    <button></button>
+                    </div>
+                </div>
+                
+            </body>
+            <footer>
+
+            </footer>
+        </div>
+    )
+  }
+  
+  export default App
