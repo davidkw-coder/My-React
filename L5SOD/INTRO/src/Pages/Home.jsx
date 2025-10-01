245b89941021026c99c94be4c2be0ef6b00b48da
Andyish-tech <ishmukazi@gmail.com>
2025-10-01 11:01:00 +0200

    UseEffect
    API fetching using Get

--- L5SOD/INTRO/src/Pages/Home.jsx
+++ L5SOD/INTRO/src/Pages/Home.jsx
@@ -1,16 +1,59 @@
-import React, {useState} from "react"
 
-function Home(){
+{/* useState this update the data
+
+    import React, {useState} from "react"
+
+    function Home(){
     const [color, setColor] = useState('Blue');
     return(
         <>
+
+        
         <h1>My color is {color}</h1>
         <button onClick={()=>{
             setColor("Yellow");
         }
         }>change Color</button>
+        <br /><br />
+
+        <input type="text" placeholder="Name"/><br /><br />
+        <input type="text" placeholder="Email" />
         </>
     )
 }
 
+export default Home
+    */}
+
+
+    // {useFect }
+    import React, {useEffect, useState} from 'react'
+    function Home(){
+        // const [count, setCount]= useState(0);
+        // return(
+        //     <>
+        //     <h1>My Number {count}</h1>
+        //     <button onClick={()=>
+        //         setCount(count +1)
+        //     }>Count</button>
+        //     </>
+        // )
+        const [data, setData] = useState([])
+            useEffect(async()=>{
+                try{
+                    const response = axios.get("")
+                    console.log(response);
+                }
+                catch (error){
+                    console.log(error)
+                }
+            }, [])
+
+            return(
+            <>
+
+            </>
+        )
+
+    }
 export default Home
