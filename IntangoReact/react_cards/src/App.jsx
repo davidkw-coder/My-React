7d37bb64ea2a9651dc32466806c055bc0f83a2e9
Andy <184005163+davidkw-coder@users.noreply.github.com>
2025-10-21 14:36:37 +0200

    Intango Components

---
+++ IntangoReact/react_cards/src/App.jsx
@@ -0,0 +1,92 @@
+
+
+function App() {
+
+  return(
+    <>
+    <div className='flex gap-10 justify-center'>
+    <div className='border-[#93010b] border rounded-md my-10 w-90 h-70 content-center px-10 shadow-xl shadow-gray-500 '>
+      <h1 className='text-[#93010b] font-bold text-[20px]'>Software Development</h1>
+      <p className='text-gray-500'>Level 3 to Level 5 (3 years program)</p> <br />
+
+      <p className='text-gray-500'>Graduates can go to universities, polytechnics, workplace or work for themselves.</p><br />
+
+      <button className='text-[#93010b] font-semibold'>LEARN MORE</button>
+    </div>
+
+
+    <div className='border-[#93010b] border rounded-md my-10 w-90 h-70 content-center px-10 shadow-xl shadow-gray-500'>
+      <h1 className='text-[#93010b] font-bold text-[20px]'>Computer Application</h1>
+      <p className='text-gray-500'>Level 2 (1 year program)</p> <br />
+
+      <p className='text-gray-500'>Graduates can go to Level 3 Software Development, advanced IT trainings, workplace or work for themselves.</p><br />
+
+      <button className='text-[#93010b] font-semibold'>LEARN MORE</button>
+    </div>
+
+
+    <div className='border-[#93010b] border rounded-md my-10 w-90 h-70 content-center px-10 shadow-xl shadow-gray-500'>
+      <h1 className='text-[#93010b] font-bold text-[20px]'>Other trainings</h1>
+      <p className='text-gray-500'>Upskilling in ICT</p> <br />
+
+      <p className='text-gray-500'>Graduates can go to advanced IT trainings, workplace or work for themselves.</p><br />
+
+      <button className='text-[#93010b] font-semibold'>LEARN MORE</button>
+    </div>
+    
+    </div>
+
+    <div className="justify-items-center">
+      <div className="w-[450px] content-center ">
+        <h3 className="text-[#93010b] text-[30px] text-center font-semibold ">Intango TSS equip students with essential knowledge, skills, and attitudes</h3>
+      </div><br /><br />
+      <div className="flex gap-40">
+      <div>
+        <div className="w-60">
+          <h4 className="text-[20px] text-[#93010b] text-center font-semibold">Knowledge Acquisition</h4>
+          <p className="text-center text-gray-500">This includes:Technical Concepts, Standards, Problem-Solving, and Safety Procedures</p>
+        </div>
+      </div>
+      <div>
+        <div className="w-60">
+          <h4 className="text-[20px] text-[#93010b] text-center font-semibold">Knowledge Acquisition</h4>
+          <p className="text-center text-gray-500">This includes:Technical Concepts, Standards, Problem-Solving, and Safety Procedures</p>
+        </div>
+      </div>
+      <div>
+        <div className="w-60">
+          <h4 className="text-[20px] text-[#93010b] text-center font-semibold">Knowledge Acquisition</h4>
+          <p className="text-center text-gray-500">This includes:Technical Concepts, Standards, Problem-Solving, and Safety Procedures</p>
+        </div>
+      </div>
+      </div>
+    </div>
+
+
+    <div className="justify-items-center">
+      <div className="w-[490px] content-center">
+       <h3 className="text-[#93010b] text-[30px] text-center font-semibold">Intango Technical Secondary School (INTANGO TSS)</h3>
+      </div> <br />
+      <div className="flex gap-15 justify-center">
+        <div className="w-[30%]">
+          <h4 className="text-[20px] text-[#93010b] font-semibold">Dear students, parents, and community members</h4>
+          <p className="text-gray-500">Intango TSS is where education meets innovation and opportunity. As the head teacher of this vibrant learning community, I am delighted to extend a warm invitation to explore all that our school has to offer.</p><br />
+
+          <p className="text-gray-500">At Intango TSS, we believe in nurturing each student's potential through a comprehensive and dynamic educational experience. Our commitment to excellence is evident in our state-of-the-art facilities, dedicated staff, and a curriculum that bridges the gap between theory and practical application.</p><br />
+
+          <p className="text-gray-500">Choosing the right school is a pivotal decision, and we are here to support you every step of the way. If you have any questions or wish to schedule a visit, please do not hesitate to contact us. We are excited to share the unique opportunities that await your child at Intango TSS.</p><br />
+
+          <p className="text-gray-500">MURANGWA Aimable,</p>
+          <p className="text-gray-500">School Head Teacher</p>
+        </div>
+        <div className="w-130 h-130">
+          <img src="/Office.png" alt="Principle's_office" className="w-130 h-130" />
+        </div>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
